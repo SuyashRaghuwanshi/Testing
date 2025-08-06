68f5e8b14648236307bc68eb3ba7a4596c9448d7
SuyashRaghuwanshi <suyashraghuwanshi322004@gmail.com>
2025-08-06 18:22:00 +0530

    Ignore .env file and remove from tracking

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
